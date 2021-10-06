44b83fb7a41d34d514c3d7bb45ed773c3ab624bf
Rico Derks <r.j.e.derks@lumc.nl>
2021-10-06 10:30:33 +0200

    There was still a small issue with RSD calculation. Fixed now.

--- R/calc_rsd.R
+++ R/calc_rsd.R
@@ -14,17 +14,35 @@
 #' @author Rico Derks
 #'
 calc_rsd <- function(lipid_data) {
-  # create long table
+  # check if there are QCpool present
   qc_df <- lipid_data %>%
     filter(grepl(x = .data$sample_type,
-                 pattern = "[qQ][cC][pP][oO][oO][lL]")) %>%
-    group_by(.data$my_id, .data$polarity) %>%
-    summarise(rsd_area = sd(.data$area, na.rm = TRUE) / mean(.data$area, na.rm = TRUE),
-              LipidClass = .data$LipidClass[1],
-              ShortLipidName = .data$ShortLipidName[1],
-              LongLipidName = .data$LongLipidName[1],
-              class_ion = .data$class_ion[1]) %>%
-    ungroup()
+                 pattern = "[qQ][cC][pP][oO][oO][lL]"))
+  if(nrow(qc_df) > 0) {
+    # QCpool present so calculate RSD values
+    qc_df <- qc_df %>%
+      group_by(.data$my_id, .data$polarity) %>%
+      summarise(rsd_area = sd(.data$area, na.rm = TRUE) / mean(.data$area, na.rm = TRUE),
+                LipidClass = .data$LipidClass[1],
+                ShortLipidName = .data$ShortLipidName[1],
+                LongLipidName = .data$LongLipidName[1],
+                class_ion = .data$class_ion[1]) %>%
+      ungroup()
+  } else {
+    # no QCpool, set RSD values to 0
+    qc_df <- lipid_data %>%
+      group_by(.data$my_id, .data$polarity) %>%
+      summarise(rsd_area = 0,
+                LipidClass = .data$LipidClass[1],
+                ShortLipidName = .data$ShortLipidName[1],
+                LongLipidName = .data$LongLipidName[1],
+                class_ion = .data$class_ion[1]) %>%
+      ungroup()
+  }
+
+  # create long table
+
+
 
   return(qc_df)
 }
